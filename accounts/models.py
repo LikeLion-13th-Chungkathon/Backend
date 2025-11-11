749fde2508a28d9781dcc85c7623b5d9d423bd41
brian <kimchanjoong54@gmail.com>
2025-11-11 18:12:18 +0900

    feat: JWT 용 custom 유저 테이블 로직 구현

--- accounts/models.py
+++ accounts/models.py
@@ -3,4 +3,5 @@ from django.contrib.auth.base_user import AbstractUser
 
 
 class User(AbstractUser):
-    pass    
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
